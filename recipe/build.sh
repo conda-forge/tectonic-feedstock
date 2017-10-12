2de6e4c1bc4a1ad8b29e097c5936b0ca2634e98e
Peter Williams <peter@newton.cx>
2017-10-11 20:40:21 -0400

    Use a somewhat nicer way to get rustc the right rpath flags [ci skip] [skip ci]

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,16 +4,11 @@ set -e
 
 if [ $(uname) = Darwin ] ; then
     export CXXFLAGS="-arch $OSX_ARCH -stdlib=libc++ -std=c++11"
-    rustc_args=(
-        -C link-args="-Wl,-rpath,$PREFIX/lib"
-    )
+    export RUSTFLAGS="-C link-args=-Wl,-rpath,$PREFIX/lib"
 else
-    rustc_args=(
-        -C link-args="-Wl,-rpath-link,$PREFIX/lib"
-    )
+    export RUSTFLAGS="-C link-args=-Wl,-rpath-link,$PREFIX/lib"
 fi
 
-cargo build --release --lib --verbose
-cargo rustc --bin tectonic --release -- "${rustc_args[@]}"
+cargo build --release --verbose
 cargo install --bin tectonic --root $PREFIX
 rm -f $PREFIX/.crates.toml
