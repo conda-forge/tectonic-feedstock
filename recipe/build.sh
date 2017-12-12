8bcefbbbda737f653b65332251429e34772c5032
Peter Williams <peter@newton.cx>
2017-12-12 08:56:15 -0500

    recipe/build.sh: work around conda-build SSL problem

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+# 2017 Dec 4: Work around SSL problem; see https://github.com/conda-forge/mosfit-feedstock/issues/23
+unset REQUESTS_CA_BUNDLE
+unset SSL_CERT_FILE
+
 if [ $(uname) = Darwin ] ; then
     export CXXFLAGS="-arch $OSX_ARCH -stdlib=libc++ -std=c++11"
     export RUSTFLAGS="-C link-args=-Wl,-rpath,$PREFIX/lib"
