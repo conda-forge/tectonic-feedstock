1db8479266aa3b0df6102273193d5b9fc11ae880
Peter Williams <peter@newton.cx>
2018-09-16 10:56:29 -0400

    Hacks to build on Travis
    Strange things are afoot at the Circle K. First of all, it is still true that
    we need to use rustc 1.26 in order to avoid stack overflow problems on Travis.
    Second, something totally bizarre is happening where the macOS compilers error
    out on me unless I run them directly at the beginning of the build (?!):
    
    ```
    dyld: Symbol not found: _iconv
      Referenced from: /usr/lib/libcups.2.dylib
      Expected in: /Users/travis/miniconda3/conda-bld/tectonic_1537107322345/_build_env/lib/libiconv.2.dylib
     in /usr/lib/libcups.2.dylib
    cc: error: unable to locate xcodebuild, please make sure the path to the Xcode folder is set correctly!
    cc: error: You can set the path to the Xcode folder using /usr/bin/xcode-select -switch
    ```
    
    (Doubly weird since I have no idea why we would be linking to CUPS!)
    
    ```
    cargo:warning=sh: line 1:  3540 Trace/BPT trap: 5       /Applications/Xcode.app/Contents/Developer/usr/bin/xcodebuild -sdk / -find ar 2> /dev/null
    cargo:warning=ar: error: unable to find utility "ar", not a developer tool or in PATH
    exit code: 72
    ```

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-set -e
+set -ex
 
 # 2017 Dec 4: Work around SSL problem; see https://github.com/conda-forge/mosfit-feedstock/issues/23
 unset REQUESTS_CA_BUNDLE
@@ -9,6 +9,14 @@ unset SSL_CERT_FILE
 if [ $(uname) = Darwin ] ; then
     export CXXFLAGS="-arch $OSX_ARCH -stdlib=libc++ -std=c++11"
     export RUSTFLAGS="-C link-args=-Wl,-rpath,$PREFIX/lib"
+
+    # Well, I have no idea what's going on here. But if we don't run these
+    # programs like this, the build fails with errors about not being able to
+    # find "cc" and "ar". ???? -- PKGW 2018 Sep 16.
+    set +e
+    cc --help
+    ar --help
+    set -e
 else
     export CFLAGS="-std=gnu99 $CFLAGS"
     export RUSTFLAGS="-C link-args=-Wl,-rpath-link,$PREFIX/lib"
