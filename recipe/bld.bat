9e4c058e5af0b1470585e598df623eb51f9c3c86
Peter Williams <peter@newton.cx>
2020-08-24 20:59:24 -0400

    Freshen up the build
    Getting rid of some old hacks and adapt to a few new things. We're still
    stuck on Windows waiting for insight into how to deal with
    https://github.com/conda-forge/python-feedstock/issues/384 .

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,17 +3,10 @@
 @REM Here we ditch the -GL flag, which messes up our static libraries.
 set "CFLAGS=-MD -DGRAPHITE2_STATIC"
 set "CXXFLAGS=-MD -DGRAPHITE2_STATIC"
-
 set "PKG_CONFIG_PATH=%LIBRARY_PREFIX:\=/%/lib/pkgconfig;%LIBRARY_PREFIX:\=/%/share/pkgconfig"
-
-@REM Sigh, import library names are all over the place
-copy %LIBRARY_PREFIX%\lib\libxml2_a.lib %LIBRARY_PREFIX%\lib\xml2.lib
-
-cargo build --release --verbose
-if errorlevel 1 exit 1
 
 cargo install --path . --bin tectonic --root %LIBRARY_PREFIX%
 if errorlevel 1 exit 1
 
 del %LIBRARY_PREFIX%\.crates.toml
-del %LIBRARY_PREFIX%\lib\xml2.lib
+del %LIBRARY_PREFIX%\.crates2.json
