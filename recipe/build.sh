8ab56c1960b0dd0456e929918ca2a4778db8f0a6
Peter Williams <peter@newton.cx>
2020-08-30 16:08:21 -0400

    recipe/build.sh: need some environment futzing to run the tests

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,12 +9,21 @@ unset SSL_CERT_FILE
 if [ $(uname) = Darwin ] ; then
     export CXXFLAGS="-arch $OSX_ARCH -stdlib=libc++ -std=c++11"
     export RUSTFLAGS="-C link-args=-Wl,-rpath,$PREFIX/lib"
+
+    # Note: the hope is that if we test in --release mode, we can avoid
+    # rebuilding everything for the `cargo install` command, but in practice
+    # this seems not to work. From local investigation it *seems* that the
+    # environment variables are not responsible, but I'm not sure exactly how
+    # cargo is deciding if/when rebuilds are needed.
+    DYLD_LIBRARY_PATH=$PREFIX/lib RUSTDOCFLAGS="-C linker=$CC" cargo test --release
 else
     export CFLAGS="-std=gnu99 $CFLAGS"
     export RUSTFLAGS="-C link-args=-Wl,-rpath-link,$PREFIX/lib"
+
+    LD_LIBRARY_PATH=$PREFIX/lib RUSTDOCFLAGS="-C linker=$CC" cargo test --release
 fi
 
-cargo test --release
+
 cargo install --path . --bin tectonic --root $PREFIX
 rm -f $PREFIX/.crates.toml
 rm -f $PREFIX/.crates2.json
