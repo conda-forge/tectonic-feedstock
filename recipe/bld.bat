bfa286defc99b8d70010b2e81be6b5891ae39273
Peter Williams <peter@newton.cx>
2020-09-08 09:02:45 -0400

    Run the test suite during the build process.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,6 +5,11 @@ set "CFLAGS=-MD -DGRAPHITE2_STATIC"
 set "CXXFLAGS=-MD -DGRAPHITE2_STATIC"
 set "PKG_CONFIG_PATH=%LIBRARY_PREFIX:\=/%/lib/pkgconfig;%LIBRARY_PREFIX:\=/%/share/pkgconfig"
 
+@REM Need to single-thread tests on Windows to avoid a filesystem locking issue.
+set RUST_TEST_THREADS=1
+cargo test --release
+if errorlevel 1 exit 1
+
 cargo install --path . --bin tectonic --root %LIBRARY_PREFIX%
 if errorlevel 1 exit 1
 
