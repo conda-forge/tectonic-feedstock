6a272f5f6e21ead60ec15232d19d071ca5edbbe4
Peter Williams <peter@newton.cx>
2023-08-25 16:43:54 -0400

    recipe/build.sh: avoid the redundant rebuild

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,12 +8,6 @@ if [ $(uname) = Darwin ] ; then
     export CXXFLAGS="-arch $OSX_ARCH -stdlib=libc++ -std=c++11"
     export RUSTFLAGS="-C link-args=-Wl,-rpath,$PREFIX/lib"
 
-    # Note: the hope is that if we test in --release mode, we can avoid
-    # rebuilding everything for the `cargo install` command, but in practice
-    # this seems not to work. From local investigation it *seems* that the
-    # environment variables are not responsible, but I'm not sure exactly how
-    # cargo is deciding if/when rebuilds are needed.
-
     if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" ]]; then
         DYLD_FALLBACK_LIBRARY_PATH=$PREFIX/lib RUSTDOCFLAGS="-C linker=$CC" cargo test --release
     fi
@@ -27,6 +21,6 @@ else
 fi
 
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
-cargo install --path . --bin tectonic --root $PREFIX
+cargo install --locked --no-track --path . --bin tectonic --root $PREFIX
 rm -f $PREFIX/.crates.toml
 rm -f $PREFIX/.crates2.json
