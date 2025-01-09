25bdb8239af4e9dd43cf763fd16371af52f9948d
Andre Anjos <andre.dos.anjos@gmail.com>
2025-01-09 06:37:25 +0100

    Support c++17 by backporting tectonic-typesetting/tectonic#1202

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,7 +2,7 @@
 
 @REM Here we ditch the -GL flag, which messes up our static libraries.
 set "CFLAGS=-MD -DGRAPHITE2_STATIC"
-set "CXXFLAGS=-MD -DGRAPHITE2_STATIC"
+set "CXXFLAGS=-MD -DGRAPHITE2_STATIC -std:c++17"
 set "PKG_CONFIG_PATH=%LIBRARY_PREFIX:\=/%/lib/pkgconfig;%LIBRARY_PREFIX:\=/%/share/pkgconfig"
 
 @REM FIX UP LIBZ-SYS
@@ -14,10 +14,10 @@ cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 @REM Also need to skip the Unicode filename test due to conda-build issue:
 @REM  https://github.com/conda/conda-build/issues/4043
 set RUST_TEST_THREADS=1
-cargo test --release -- --skip unicode_file_name
+cargo test --release --features external-harfbuzz -- --skip unicode_file_name
 if errorlevel 1 exit 1
 
-cargo install --path . --bin tectonic --root %LIBRARY_PREFIX%
+cargo install --path . --bin tectonic --root %LIBRARY_PREFIX% --features external-harfbuzz
 if errorlevel 1 exit 1
 
 del %LIBRARY_PREFIX%\.crates.toml
